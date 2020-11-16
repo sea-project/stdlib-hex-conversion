24cf99bf6cde0c3598218da0fdb6c586f87eec6d
biwow <biwow@qq.com>
2020-11-16 20:21:39 +0800

    保留36进制转换

--- dec.go
+++ dec.go
@@ -1,147 +1,29 @@
 package stdlib
 
 import (
-	"math"
-	"strconv"
-	"strings"
+	"math/big"
 )
 
-var tenToAny map[int]string = map[int]string{
-	0:  "0",
-	1:  "1",
-	2:  "2",
-	3:  "3",
-	4:  "4",
-	5:  "5",
-	6:  "6",
-	7:  "7",
-	8:  "8",
-	9:  "9",
-	10: "a",
-	11: "b",
-	12: "c",
-	13: "d",
-	14: "e",
-	15: "f",
-	16: "g",
-	17: "h",
-	18: "i",
-	19: "j",
-	20: "k",
-	21: "l",
-	22: "m",
-	23: "n",
-	24: "o",
-	25: "p",
-	26: "q",
-	27: "r",
-	28: "s",
-	29: "t",
-	30: "u",
-	31: "v",
-	32: "w",
-	33: "x",
-	34: "y",
-	35: "z",
-	36: "A",
-	37: "B",
-	38: "C",
-	39: "D",
-	40: "E",
-	41: "F",
-	42: "G",
-	43: "H",
-	44: "I",
-	45: "J",
-	46: "K",
-	47: "L",
-	48: "M",
-	49: "N",
-	50: "O",
-	51: "P",
-	52: "Q",
-	53: "R",
-	54: "S",
-	55: "T",
-	56: "U",
-	57: "V",
-	58: "W",
-	59: "X",
-	60: "Y",
-	61: "Z"}
+var (
+	Big0        = big.NewInt(0)
+	Big36       = big.NewInt(36)
+	Base62Chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+)
 
-// 10进制转任意进制
-func decimalToAny(num, n int) string {
-	new_num_str := ""
-	var remainder int
-	var remainder_string string
-	for num != 0 {
-		remainder = num % n
-		if 76 > remainder && remainder > 9 {
-			remainder_string = tenToAny[remainder]
-		} else {
-			remainder_string = strconv.Itoa(remainder)
-		}
-		new_num_str = remainder_string + new_num_str
-		num = num / n
-	}
-	return new_num_str
-}
-
-// map根据value找key
-func findKey(in string) int {
-	result := -1
-	for k, v := range tenToAny {
-		if in == v {
-			result = k
-		}
-	}
-	return result
-}
-
-// 任意进制转10进制
-func anyToDecimal(num string, n int) int {
-	var new_num float64
-	new_num = 0.0
-	nNum := len(strings.Split(num, "")) - 1
-	for _, value := range strings.Split(num, "") {
-		tmp := float64(findKey(value))
-		if tmp != -1 {
-			new_num = new_num + tmp*math.Pow(float64(n), float64(nNum))
-			nNum = nNum - 1
-		} else {
+func BHex2DEC(i *big.Int) string {
+	var chars []rune
+	x := new(big.Int)
+	for {
+		x.Mod(i, Big36)
+		chars = append(chars, rune(Base62Chars[x.Uint64()]))
+		i.Div(i, Big36)
+		if i.Cmp(Big0) == 0 {
 			break
 		}
 	}
-	return int(new_num)
-}
-
-// DECToBIN 十进制转二进制
-func DECToBIN(n int) string {
-	return decimalToAny(n, 2)
-}
-
-// BINToDEC 二进制转十进制
-func BINToDEC(n string) int {
-	return anyToDecimal(n, 2)
-}
-
-// DECToHEX 十进制转十六进制
-func DECToHEX(n int) string {
-	return decimalToAny(n, 16)
-}
-
-// HEXToDEC 十六进制转十进制
-func HEXToDEC(n string) int {
-	return anyToDecimal(n, 16)
-}
-
-// DECToBHex 十进制转三十六进制
-func DECToBHex(n int) string {
-	return decimalToAny(n, 36)
-}
-
-// BHexToDEC 三十六进制转十进制
-func BHexToDEC(n string) int {
-	return anyToDecimal(n, 36)
+	// reverse slice
+	for i, j := 0, len(chars)-1; i < j; i, j = i+1, j-1 {
+		chars[i], chars[j] = chars[j], chars[i]
+	}
+	return string(chars)
 }
--- dec_test.go
+++ dec_test.go
@@ -1,10 +1,12 @@
 package stdlib
 
-import "testing"
+import (
+	"math/big"
+	"testing"
+)
 
 func Test_DEC(t *testing.T) {
-	a := 37
-	t.Log(DECToBIN(a))
-	t.Log(DECToHEX(a))
-	t.Log(DECToBHex(a))
+	a := big.NewInt(37)
+	t.Log(BHex2DEC(a))
+
 }
